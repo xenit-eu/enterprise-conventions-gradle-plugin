b8fe8bd9645d46d9a71a7a95813af5934931e2b2
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-11-25 11:39:10 +0100

    Handle change of AuthenticationSupportedInternal#getConfiguredCredentials() in Gradle 6.6

--- src/main/java/eu/xenit/gradle/enterprise/publish/OssPublishPlugin.java
+++ src/main/java/eu/xenit/gradle/enterprise/publish/OssPublishPlugin.java
@@ -2,27 +2,59 @@ package eu.xenit.gradle.enterprise.publish;
 
 import eu.xenit.gradle.enterprise.repository.BlockedRepositoryException;
 import eu.xenit.gradle.enterprise.violations.ViolationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.credentials.Credentials;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.gradle.api.provider.Property;
 import org.gradle.api.publish.PublishingExtension;
 import org.gradle.internal.artifacts.repositories.AuthenticationSupportedInternal;
 import org.gradle.plugins.signing.SigningExtension;
 import org.gradle.plugins.signing.SigningPlugin;
+import org.gradle.util.GradleVersion;
 
 public class OssPublishPlugin extends AbstractPublishPlugin {
+
+    private Logger LOGGER = Logging.getLogger(OssPublishPlugin.class);
 
     @Override
     protected void validatePublishRepository(ViolationHandler violationHandler, MavenArtifactRepository repository) {
         if (repository.getUrl().getScheme().equals("http")) {
-            boolean hasCredentials = true;
-            if (repository instanceof AuthenticationSupportedInternal) {
-                hasCredentials = ((AuthenticationSupportedInternal) repository).getConfiguredCredentials() != null;
-            }
+            boolean hasCredentials = checkHasCredentials(repository);
             if (hasCredentials) {
                 violationHandler.handleViolation(new BlockedRepositoryException(repository.getUrl(),
                         "Publishing to HTTP repositories with credentials is not allowed."));
             }
         }
+    }
+
+    private boolean checkHasCredentials(MavenArtifactRepository repository) {
+        try {
+            return checkHasCredentials0(repository);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            LOGGER.warn(
+                    "Failed to determine if repository has credentials and is HTTP. Continuing as if no credentials are configured.");
+            return false;
+        }
+    }
+
+    private boolean checkHasCredentials0(MavenArtifactRepository repository)
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        if (repository instanceof AuthenticationSupportedInternal) {
+            Method method = repository.getClass().getMethod("getConfiguredCredentials");
+            // Internals have changed starting from Gradle 6.6
+            if (GradleVersion.current().compareTo(GradleVersion.version("6.6")) < 0) {
+                return method.invoke(repository) != null;
+            } else {
+                Property<Credentials> credentialsProperty = (Property<Credentials>) method.invoke(repository);
+                return credentialsProperty.isPresent();
+            }
+        }
+
+        return true;
     }
 
     @Override
