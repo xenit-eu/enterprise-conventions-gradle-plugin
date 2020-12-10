3fcbd5b52cb54608c05869aa9daa45859cc00a54
Lars Vierbergen <lars.vierbergen@xenit.eu>
2020-12-10 23:17:01 +0100

    Ensure that repository replacement cache lock gets released after using it

--- src/main/java/eu/xenit/gradle/enterprise/conventions/internal/artifactory/CachingArtifactoryClient.java
+++ src/main/java/eu/xenit/gradle/enterprise/conventions/internal/artifactory/CachingArtifactoryClient.java
@@ -8,6 +8,7 @@ import javax.annotation.Nullable;
 import org.gradle.api.GradleException;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.Logging;
+import org.gradle.cache.CacheBuilder;
 import org.gradle.cache.CacheBuilder.LockTarget;
 import org.gradle.cache.CacheRepository;
 import org.gradle.cache.FileLockManager.LockMode;
@@ -26,8 +27,6 @@ public class CachingArtifactoryClient implements ArtifactoryClient {
     private final ArtifactoryClient client;
 
     private final CacheRepository cacheRepository;
-    private PersistentCache persistentCache;
-    private PersistentIndexedCache<String, RepositoriesAndDate> repositoryMapCache;
     private final String cacheKey;
     private final boolean offline;
 
@@ -39,58 +38,49 @@ public class CachingArtifactoryClient implements ArtifactoryClient {
         this.offline = offline;
     }
 
-    private PersistentCache getPersistentCache() {
-        if (persistentCache == null) {
-            persistentCache = cacheRepository.cache("eu.xenit.gradle.enterprise.conventions")
-                    .withCrossVersionCache(LockTarget.DefaultTarget)
-                    .withLockOptions(LockOptionsBuilder.mode(LockMode.Exclusive))
-                    .withDisplayName("eu.xenit.enterprise-conventions repository replacement cache")
-                    .withProperties(Collections.singletonMap("cacheVersion", "3"))
-                    .open();
-        }
-        return persistentCache;
+    private CacheBuilder createCacheBuilder() {
+        return cacheRepository.cache("eu.xenit.gradle.enterprise.conventions")
+                .withCrossVersionCache(LockTarget.DefaultTarget)
+                .withLockOptions(LockOptionsBuilder.mode(LockMode.Exclusive))
+                .withDisplayName("eu.xenit.enterprise-conventions repository replacement cache")
+                .withProperties(Collections.singletonMap("cacheVersion", "3"));
     }
-
-    private PersistentIndexedCache<String, RepositoriesAndDate> getIndexedCache() {
-        if (repositoryMapCache == null) {
-            PersistentCache cache = getPersistentCache();
-            repositoryMapCache = cache.createCache(CACHE_PARAMETERS);
-        }
-        return repositoryMapCache;
-    }
-
 
     @Override
     public List<ArtifactoryRepositorySpec> getRepositories() {
-        PersistentCache cache = getPersistentCache();
-        @Nullable
-        List<ArtifactoryRepositorySpec> cachedRepositories = cache.useCache(() -> {
-            RepositoriesAndDate repositoriesAndDate = null;
-            try {
-                repositoriesAndDate = getIndexedCache().get(this.cacheKey);
-            } catch (Exception e) {
-                LOGGER.error("Failed to load cached repository information", e);
-            }
-            if (repositoriesAndDate == null) {
+        CacheBuilder cacheBuilder = createCacheBuilder();
+        try (PersistentCache cache = cacheBuilder.open()) {
+            @Nullable
+            List<ArtifactoryRepositorySpec> cachedRepositories = cache.useCache(() -> {
+                RepositoriesAndDate repositoriesAndDate = null;
+                try {
+                    PersistentIndexedCache<String, RepositoriesAndDate> repositoryMapCache = cache
+                            .createCache(CACHE_PARAMETERS);
+                    repositoriesAndDate = repositoryMapCache.get(this.cacheKey);
+                } catch (Exception e) {
+                    LOGGER.error("Failed to load cached repository information", e);
+                }
+                if (repositoriesAndDate == null) {
+                    return null;
+                }
+
+                Instant now = Instant.now();
+                Instant expiry = Instant.ofEpochSecond(repositoriesAndDate.getExpirySeconds());
+
+                LOGGER.debug("Cache expires at {}", expiry);
+
+                if (expiry.isAfter(now)) {
+                    LOGGER.debug("Cache is still valid for {}", Duration.between(now, expiry));
+                    return Collections.unmodifiableList(repositoriesAndDate.getRepositories());
+                } else if (this.offline) {
+                    LOGGER.warn("Gradle is running in offline mode. Using stale cached repository information");
+                    return Collections.unmodifiableList(repositoriesAndDate.getRepositories());
+                }
                 return null;
+            });
+            if (cachedRepositories != null) {
+                return cachedRepositories;
             }
-
-            Instant now = Instant.now();
-            Instant expiry = Instant.ofEpochSecond(repositoriesAndDate.getExpirySeconds());
-
-            LOGGER.debug("Cache expires at {}", expiry);
-
-            if (expiry.isAfter(now)) {
-                LOGGER.debug("Cache is still valid for {}", Duration.between(now, expiry));
-                return Collections.unmodifiableList(repositoriesAndDate.getRepositories());
-            } else if (this.offline) {
-                LOGGER.warn("Gradle is running in offline mode. Using stale cached repository information");
-                return Collections.unmodifiableList(repositoriesAndDate.getRepositories());
-            }
-            return null;
-        });
-        if (cachedRepositories != null) {
-            return cachedRepositories;
         }
 
         if (this.offline) {
@@ -100,17 +90,21 @@ public class CachingArtifactoryClient implements ArtifactoryClient {
         LOGGER.info("Fetching repository information with client {}", client);
         List<ArtifactoryRepositorySpec> repositories = client.getRepositories();
 
-        cache.useCache(() -> {
-            RepositoriesAndDate repositoriesAndDate = new RepositoriesAndDate();
-            repositoriesAndDate.setExpirySeconds(Instant.now().getEpochSecond() + VALIDITY_SECONDS);
-            repositoriesAndDate.setRepositories(repositories);
-            LOGGER.debug("Storing repositories {} into cache", repositories);
-            try {
-                getIndexedCache().put(this.cacheKey, repositoriesAndDate);
-            } catch (Exception e) {
-                LOGGER.error("Failed to store cached repository information", e);
-            }
-        });
+        try (PersistentCache cache = cacheBuilder.open()) {
+            cache.useCache(() -> {
+                RepositoriesAndDate repositoriesAndDate = new RepositoriesAndDate();
+                repositoriesAndDate.setExpirySeconds(Instant.now().getEpochSecond() + VALIDITY_SECONDS);
+                repositoriesAndDate.setRepositories(repositories);
+                LOGGER.debug("Storing repositories {} into cache", repositories);
+                try {
+                    PersistentIndexedCache<String, RepositoriesAndDate> repositoryMapCache = cache
+                            .createCache(CACHE_PARAMETERS);
+                    repositoryMapCache.put(this.cacheKey, repositoriesAndDate);
+                } catch (Exception e) {
+                    LOGGER.error("Failed to store cached repository information", e);
+                }
+            });
+        }
 
         return Collections.unmodifiableList(repositories);
     }
