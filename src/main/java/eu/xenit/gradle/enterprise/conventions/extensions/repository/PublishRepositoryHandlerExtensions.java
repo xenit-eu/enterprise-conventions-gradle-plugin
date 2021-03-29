dfcf7bba19ebae0fb43c60af1ee9a52900d8af69
Lars Vierbergen <lars.vierbergen@xenit.eu>
2021-03-29 17:46:50 +0200

    Replace application of NexusPublishPlugin with creating its configuration and initialize task by ourself

--- src/main/java/eu/xenit/gradle/enterprise/conventions/extensions/repository/PublishRepositoryHandlerExtensions.java
+++ src/main/java/eu/xenit/gradle/enterprise/conventions/extensions/repository/PublishRepositoryHandlerExtensions.java
@@ -1,10 +1,11 @@
 package eu.xenit.gradle.enterprise.conventions.extensions.repository;
 
+import de.marcphilipp.gradle.nexus.InitializeNexusStagingRepository;
 import de.marcphilipp.gradle.nexus.NexusPublishExtension;
-import de.marcphilipp.gradle.nexus.NexusPublishPlugin;
 import de.marcphilipp.gradle.nexus.NexusRepository;
 import eu.xenit.gradle.enterprise.conventions.api.PluginApi;
 import eu.xenit.gradle.enterprise.conventions.api.PublicApi;
+import java.util.HashMap;
 import javax.inject.Inject;
 import kotlin.text.StringsKt;
 import org.gradle.api.Action;
@@ -12,16 +13,20 @@ import org.gradle.api.Project;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
 import org.gradle.api.internal.HasConvention;
+import org.gradle.api.publish.maven.tasks.PublishToMavenRepository;
+import org.gradle.api.tasks.TaskProvider;
 
 public class PublishRepositoryHandlerExtensions {
 
     private static final Action<? super MavenArtifactRepository> EMPTY_ACTION = repository -> {
     };
     private final Project project;
+    private final RepositoryHandler repositoryHandler;
 
     @Inject
-    public PublishRepositoryHandlerExtensions(Project project) {
+    public PublishRepositoryHandlerExtensions(Project project, RepositoryHandler repositoryHandler) {
         this.project = project;
+        this.repositoryHandler = repositoryHandler;
     }
 
     @PublicApi
@@ -31,38 +36,48 @@ public class PublishRepositoryHandlerExtensions {
 
     @PublicApi
     public MavenArtifactRepository sonatypeMavenCentral(Action<? super MavenArtifactRepository> action) {
-        project.getPlugins().apply(NexusPublishPlugin.class);
-        NexusPublishExtension nexusPublishExtension = project.getExtensions().getByType(NexusPublishExtension.class);
-
-        // Get rid of tasks automatically added by the NexusPublishPlugin.
-        // These do not contribute anything to the core functionality, which is creating a temporary repository
-        // to publish releases to.
-        // We did not ask for and do not want these additional tasks, because they may be confusing to users.
-        // We can't just delete them, because that would cause problems with deferred configuration of that plugin
-        // But we can at least disable them and move them to the other group
-        nexusPublishExtension.getRepositories().whenObjectAdded(nexusRepository -> {
-            project.getTasks().named("publishTo" + StringsKt.capitalize(nexusRepository.getName())).configure(task -> {
-                task.setEnabled(false);
-                task.setGroup(null);
-            });
-        });
-
+        // Create a publish extension for the InitializeNexusStagingRepository task
         // Always use staging, as staging is required for publishing to sonatype releases
+        NexusPublishExtension nexusPublishExtension = project.getObjects()
+                .newInstance(NexusPublishExtension.class, project);
         nexusPublishExtension.getUseStaging().set(true);
 
         // Create sonatype artifact
-        NexusRepository sonatypeNexus = nexusPublishExtension.getRepositories().sonatype();
-        MavenArtifactRepository artifactRepository = project.getObjects()
-                .newInstance(SonatypeMavenCentralPublishRepository.class, sonatypeNexus);
+        NexusRepository nexusRepository = nexusPublishExtension.getRepositories().sonatype();
+        MavenArtifactRepository mavenRepository = repositoryHandler.maven(repo -> {
+            repo.setName(nexusRepository.getName());
+            repo.setUrl(nexusRepository.getNexusUrl());
+            action.execute(repo);
+        });
 
-        action.execute(artifactRepository);
+        // Configure nexus repository username & password from maven repository username & password
+        nexusRepository.getUsername().set(project.provider(() -> mavenRepository.getCredentials().getUsername()));
+        nexusRepository.getPassword().set(project.provider(() -> mavenRepository.getCredentials().getPassword()));
 
-        return artifactRepository;
+        // Create task to create staging repositories
+        TaskProvider<InitializeNexusStagingRepository> initializeStaging = project.getTasks()
+                .register(
+                        "initialize" + StringsKt.capitalize(nexusRepository.getName()) + "StagingRepository",
+                        InitializeNexusStagingRepository.class,
+                        project.getObjects(),
+                        nexusPublishExtension,
+                        nexusRepository,
+                        new HashMap<>() // Only used once
+                );
+
+        // Make publish task depend on creating the staging repository
+        project.getTasks().withType(PublishToMavenRepository.class).configureEach(publishTask -> {
+            if (publishTask.getRepository().equals(mavenRepository)) {
+                publishTask.dependsOn(initializeStaging);
+            }
+        });
+
+        return mavenRepository;
     }
 
     static void apply(RepositoryHandler repositoryHandler, Project project) {
         PublishRepositoryHandlerExtensions extensions = project.getObjects()
-                .newInstance(PublishRepositoryHandlerExtensions.class, project);
+                .newInstance(PublishRepositoryHandlerExtensions.class, project, repositoryHandler);
 
         ((HasConvention) repositoryHandler).getConvention().getPlugins()
                 .put(PublishRepositoryHandlerExtensions.class.getCanonicalName(), extensions);
